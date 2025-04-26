7fa726bc509ede769d579132b5396faf86cc69f0
Zadfar <zadfarzubair55@gmail.com>
2025-04-26 16:32:41 +0530

    made it responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,30 @@ import Viewport from './components/Viewport';
 import UploadButton from './components/UploadButton';
 import DeleteButton from './components/DeleteButton';
 import { useModelStore } from './store/model';
+import MenuIcon from './assets/menu.svg'
+import CrossIcon from './assets/cross.svg'
+import { useState } from 'react';
 
 
 const App = () => {
   const { useGrid, toggleUseGrid, useAnimation, toggleUseAnimation } = useModelStore();
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  }
+
   return (
     <>
       <div className='container'>
         <div className='dashboard'>
           <div className='header'>
             <h1>3D Model Viewer</h1>
+            <div className={`dashboard-btn`}>
+              <button onClick={handleClick}><img src={isVisible ? CrossIcon : MenuIcon} alt="icon" /></button>
+            </div>
           </div>
-          <div className='dashboard-items'>
+          <div className={`dashboard-items ${isVisible ? '' : 'hidden'}`}>
             <h2>Choose a Model</h2>
             <ModelList />
             <UploadButton />
