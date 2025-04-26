85083ea6075730f69801bac13383e0614f60ccc8
Zadfar <zadfarzubair55@gmail.com>
2025-04-26 18:34:03 +0530

    fixed issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api" : {
-        target: "http://localhost:5001",
+        target: "https://model-viewer-backend.onrender.com",
       }
     }
   },
